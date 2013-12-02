f882c6aa14522e03383d2804b34e1ba593047814
Jeremy Karson <jeremy.karson@gmail.com>
2013-12-01 22:49:09 -0500

    changed port # to 7007'

--- api.py
+++ api.py
@@ -247,4 +247,4 @@ def generateEnd():
 if __name__=="__main__":
     app.debug=True
     app.secret_key = 'super secret'
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=7007)
